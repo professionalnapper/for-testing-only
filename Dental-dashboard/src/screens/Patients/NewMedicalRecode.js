5e249a83fef551e391901eb4178d343c0d55d2ff
professionalnapper <quiamcocristal2829@gmail.com>
2024-04-17 23:14:25 +0800

    added changes

--- Dental-dashboard/src/screens/Patients/NewMedicalRecode.js
+++ Dental-dashboard/src/screens/Patients/NewMedicalRecode.js
@@ -78,13 +78,13 @@ function NewMedicalRecode() {
           className="col-span-12 flex-colo gap-6 lg:col-span-4 bg-white rounded-xl border-[1px] border-border p-6 lg:sticky top-28"
         >
           <img
-            src="/images/user7.png"
+            src="/images/catto.jpg"
             alt="setting"
             className="w-40 h-40 rounded-full object-cover border border-dashed border-subMain"
           />
           <div className="gap-2 flex-colo">
-            <h2 className="text-sm font-semibold">Amani Mmassy</h2>
-            <p className="text-xs text-textGray">amanimmassy@gmail.com</p>
+            <h2 className="text-sm font-semibold">yooo</h2>
+            <p className="text-xs text-textGray">yooo@gmail.com</p>
             <p className="text-xs">+254 712 345 678</p>
             <p className="text-xs text-subMain bg-text font-medium py-1 px-4 rounded-full border-[0.5px] border-subMain">
               45 yrs{' '}
