ca276b49704b18c70d74345340013b52b076db5f
professionalnapper <quiamcocristal2829@gmail.com>
2024-04-17 22:36:41 +0800

    changed the icon

--- Dental-dashboard/src/screens/SignUp.js
+++ Dental-dashboard/src/screens/SignUp.js
@@ -10,7 +10,7 @@ function SignUp() {
     <div className="w-full h-screen flex-colo bg-dry">
       <form className="w-2/5 p-8 rounded-2xl mx-auto bg-white flex-colo">
         <img
-          src="/images/logo.png"
+          src="/images/healthflow-logo.png"
           alt="logo"
           className="w-48 h-16 object-contain"
         />
