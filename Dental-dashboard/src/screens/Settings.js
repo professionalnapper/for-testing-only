4a943d976eb6807af61e7d00d45ed845c69a63d4
professionalnapper <quiamcocristal2829@gmail.com>
2024-04-17 23:06:06 +0800

    changed and added some files

--- Dental-dashboard/src/screens/Settings.js
+++ Dental-dashboard/src/screens/Settings.js
@@ -43,7 +43,7 @@ function Settings() {
           className="col-span-12 flex-colo gap-6 lg:col-span-4 bg-white rounded-xl border-[1px] border-border p-6 lg:sticky top-28"
         >
           <img
-            src="/images/user1.png"
+            src="/images/raya.jpg"
             alt="setting"
             className="w-40 h-40 rounded-full object-cover border border-dashed border-subMain"
           />
