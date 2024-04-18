b661ef09f278d246db2881e2ee0407c1797acb9b
professionalnapper <quiamcocristal2829@gmail.com>
2024-04-18 20:31:25 +0800

    changed the logo pic

--- Dental-dashboard/src/screens/Invoices/CreateInvoice.js
+++ Dental-dashboard/src/screens/Invoices/CreateInvoice.js
@@ -68,7 +68,7 @@ function CreateInvoice() {
         <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-2 items-center">
           <div className="lg:col-span-3">
             <img
-              src="/images/logo.png"
+              src="/images/healthflow-logo.png"
               alt="logo"
               className=" w-32 object-contain"
             />
